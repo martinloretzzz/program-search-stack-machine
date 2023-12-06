f38555d24c54fba7b4716b9deeac0f5487037543
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2023-12-06 17:56:03 +0700

    Add simple stack machine

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,57 @@ import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 
+const operants = ["push-a", "push-b", "push-c", "add", "subtract", "multiply", "divide"] as const;
+type Operant = (typeof operants)[number];
+
+const testProgram = (program: Operant[], data: number[]) => {
+	const stack: number[] = [];
+	for (const operant of program) {
+		switch (operant) {
+			case "push-a":
+				stack.push(data[0]);
+				break;
+			case "push-b":
+				stack.push(data[1]);
+				break;
+			case "push-c":
+				stack.push(data[2]);
+				break;
+			case "add":
+				stack.push(stack.pop()! + stack.pop()!);
+				break;
+			case "subtract":
+				stack.push(stack.pop()! - stack.pop()!);
+				break;
+			case "multiply":
+				stack.push(stack.pop()! * stack.pop()!);
+				break;
+			case "divide":
+				stack.push(stack.pop()! / stack.pop()!);
+				break;
+		}
+	}
+	console.log(stack);
+	if (stack.length !== 1) return undefined;
+	return stack.pop()!;
+};
+
+const tests = [
+	[1, 1, 1, 1],
+	[2, 2, 2, 8],
+	[3, 3, 3, 27],
+	[4, 4, 4, 64],
+];
+
+const program: Operant[] = ["push-a", "push-b", "push-c", "multiply", "multiply"];
+
+for (const test of tests) {
+	const result = testProgram(program, test);
+	if (result !== test[3]) {
+		console.log("Test failed!");
+	}
+}
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
