fe5dda93e93d7700a3aa5cbffaa2eef06acde89f
0x4d - Martin Loretz <20306567+martinloretzzz@users.noreply.github.com>
2023-12-07 15:39:58 +0700

    Add genome creation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import "./style.css";
 
-const operants = ["push-a", "push-b", "push-c", "add", "subtract", "multiply", "divide"] as const;
-type Operant = (typeof operants)[number];
+const operantMap = {
+	0: "nop",
+	1: "push-a",
+	2: "push-b",
+	3: "push-c",
+	4: "add",
+	5: "subtract",
+	6: "multiply",
+	7: "divide",
+} as const;
 
-const testProgram = (program: Operant[], data: number[]) => {
+type Operant = (typeof operantMap)[keyof typeof operantMap];
+type Genome = number[];
+type Program = Operant[];
+
+const testProgram = (program: Program, data: number[]) => {
 	const stack: number[] = [];
 	for (const operant of program) {
 		switch (operant) {
@@ -50,3 +62,61 @@ for (const test of tests) {
 		console.log("Test failed!");
 	}
 }
+
+const randomIntBetween = (min: number, max: number) => {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomPush = () => randomIntBetween(1, 3);
+const randomMathOperant = () => randomIntBetween(4, 7);
+const randomOperant = () => randomIntBetween(1, 7);
+const isPush = (op: number) => op >= 1 && op <= 3;
+const isMathOperant = (op: number) => op >= 4 && op <= 7;
+
+const generateInitialGenome = (maxProgramSize: number) => {
+	const program: Genome = [];
+	let stackPointer = 0;
+	let exit = false;
+	for (let i = 0; i < 2; i++) {
+		program.push(randomPush());
+		stackPointer += 1;
+	}
+	for (let i = 2; i < maxProgramSize; i++) {
+		if (exit) break;
+		const op = randomOperant();
+		if (isPush(op)) {
+			if (stackPointer < 3) {
+				program.push(op);
+				stackPointer += 1;
+			}
+		} else {
+			program.push(op);
+			stackPointer -= 1;
+			if (stackPointer === 1) exit = true;
+		}
+	}
+	return program;
+};
+
+const genomeToProgram = (genome: Genome): Program => {
+	return genome.map((gene) => (operantMap as any)[gene] as Operant);
+};
+
+const isProgramValid = (program: Program) => {
+	let stackPointer = 0;
+	for (const operant of program) {
+		if (operant == "nop") continue;
+		if (operant.startsWith("push-")) {
+			stackPointer++;
+		} else {
+			stackPointer -= 1;
+		}
+		return stackPointer === 1;
+	}
+};
+
+const genome = generateInitialGenome(8);
+const genomeProgram = genomeToProgram(genome);
+console.log(genome);
+console.log(genomeProgram);
+console.log(isProgramValid(genomeProgram));
